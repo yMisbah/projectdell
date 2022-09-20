f54ef620969ba5a687a2b415ff55758b70f396a9
yMisbah <83804390+yMisbah@users.noreply.github.com>
2022-09-21 03:41:45 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 const container = document.querySelector('#container')
-const panorama = new PANOLENS.ImagePanorama( 'images/pano.jpg' );
+const panorama = new PANOLENS.ImagePanorama( 'https://ymisbah.github.io/projectdell/images/pano.jpg' );
 const viewer = new PANOLENS.Viewer({container:container});
 viewer.add( panorama );
